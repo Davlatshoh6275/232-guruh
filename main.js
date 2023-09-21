540bcb8b432caf1b3e5dec49d716e07fb1403214
Davlatshoh <davlatshoh6275@gmail.com>
2023-09-21 16:27:19 +0300

    lesson js string methods

---
+++ main.js
@@ -0,0 +1,86 @@
+// let a = "10.3"
+
+// let b = parseInt(a)
+// let x = parseFloat(a)
+
+
+
+// let 
+
+// let rnd = Math.round(Math.random() * 1000)
+
+// // console.log(rnd);
+
+// let y = Math.random(2, 5, 7, 8, 46, 45)
+
+// console.log(
+//     y
+// );
+
+// o = 10
+// o = 15
+// o = "salom"
+
+// var o;
+
+// console.log(o);
+
+// p = "salom"
+// p = 48
+// p = 99
+
+// let p;
+
+// console.log(p);
+
+
+// let a = "10.2"
+
+// let b = Number(a)
+// console.log(a);
+// console.log(b);
+
+let txt = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, repudiandae minima nostrum dolorem sunt obcaecati iure temporibus accusantium ab unde illum?        Excepturi delectus consequuntur ratione architecto cum animi numquam amet?"
+
+// let q = 10
+// let a = String(q)
+
+// let uptxt = txt.toUpperCase()
+
+// let lowtxt = uptxt.toLowerCase()
+
+
+// let y = txt.replace("Lorem", "Davlatshoh")
+
+// let u = txt.trim()
+
+// // console.log(y);
+// console.log(u);
+
+// let a = txt.charAt(0)
+
+// let y = txt.split('  ')
+
+// let o = txt.includes("Lorem")
+
+// let r = txt.slice(0, 16)
+
+// let u = txt.search("ipsum")
+
+// let p = txt.concat(" Samir")
+
+// // console.log(r);
+// console.log(u);
+
+// console.log(p);
+
+// console.log(txt);
+// console.log(a);
+// console.log(y);
+// console.log(o);
+
+
+
+
+
+
